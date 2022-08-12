d74c7f81c6ae42bac25bbbfe14d1a47e637be9f7
PavanKumarGudala <pavank@leewayhertz.com>
2022-08-12 17:41:28 +0530

    build updated

---
+++ build/precache-manifest.eb1012a21a6e644488a058d8cfb520b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91f04cbea328deecbbcca9f30d3e3f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4e8c6c78df4b2ce3ad6",
+    "url": "/static/css/main.7d1c4b46.chunk.css"
+  },
+  {
+    "revision": "c48d36ad4149c37928ab",
+    "url": "/static/js/2.7a53644e.chunk.js"
+  },
+  {
+    "revision": "b4e8c6c78df4b2ce3ad6",
+    "url": "/static/js/main.7df986bc.chunk.js"
+  },
+  {
+    "revision": "46792a16ff2fd3f56d37",
+    "url": "/static/js/runtime~main.3293bef8.js"
+  },
+  {
+    "revision": "08c20a487911694291bd8c5de41315ad",
+    "url": "/static/media/Poppins-Bold.08c20a48.ttf"
+  },
+  {
+    "revision": "bf59c687bc6d3a70204d3944082c5cc0",
+    "url": "/static/media/Poppins-Medium.bf59c687.ttf"
+  },
+  {
+    "revision": "093ee89be9ede30383f39a899c485a82",
+    "url": "/static/media/Poppins-Regular.093ee89b.ttf"
+  },
+  {
+    "revision": "6f1520d107205975713ba09df778f93f",
+    "url": "/static/media/Poppins-SemiBold.6f1520d1.ttf"
+  }
+]);
